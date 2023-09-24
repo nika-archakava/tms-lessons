c8ba69c4b78731266a80f669e9b0a7c3d36f3c0f
Veranika Archakava <veranikaarchakava@gmail.com>
2023-09-24 21:42:42 +0300

    Delete this Homework_04

--- Homework_04/exercise_3.py
+++ Homework_04/exercise_3.py
@@ -1,10 +1,13 @@
-print(0)
-for i in range(1, 101):
+for i in range(101):
+    print(i)
     answer = input("Should we break? ")
     if answer == 'Yes':
         break
     elif answer == 'No':
-        print(i)
         continue
     else:
-        print("Don't undestand you")
+        while True:
+            print("Don't understand you")
+            answer = input("Should we break? ")
+            if answer == 'Yes' or answer == 'No':
+                break
